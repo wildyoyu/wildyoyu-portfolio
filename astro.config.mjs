142ceb742e29c6421fcf780426e4eeebc65d7a6c
Jorge (wildyoyu) <jorge.ordonez.mora@gmail.com>
2025-09-15 00:02:10 +0200

    deploy: versión responsive estilo IG

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://wildyoyu.net', // o tu URL de Netlify si aún no tienes dominio
+  site: 'https://wildyoyu.com', // o tu URL de Netlify si aún no tienes dominio
   scopedStyleStrategy: 'where',
   integrations: [sitemap()],
 });
